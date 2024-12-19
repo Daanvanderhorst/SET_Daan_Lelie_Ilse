76d1be212d425a33a3b1fbec0db75089927a747c
IlsevanderVen <ven.ilse06@gmail.com>
2024-12-19 15:49:05 +0100

    Update Stap 1 -class.py

--- Stap 1 -class.py
+++ Stap 1 -class.py
@@ -5,29 +5,24 @@ class number(Enum):
     num1 = 1
     num2 = 2
     num3 = 3
-    def _eq_(self, other):
-        return self.number == other.number
+
         
-class symbol(Enum): #hoi Daan
+class symbol(Enum): 
     diamont = 1
     wave = 2
     oval = 3
-    def _eq_(self, other):
-        return self.symbol == other.symbol
         
 class color(Enum):
     red = 1
     green = 2
     purple = 3
-    def _eq_(self, other):
-        return self.color == other.color
 
-class shading(Enum):#shading comment
+
+class shading(Enum):
     solid = 1
     striped = 2
     empty = 3
-    def _eq_(self, other):
-        return self.shading == other.shading
+
     
 class card:
     def _init_(self, number, symbol, color, shading):
@@ -35,20 +30,27 @@ class card:
         self.symbol = symbol
         self.color = color
         self.shading = shading
+
+    def eqnumber(self, other):
+        return self.number == other.number
+    def eqcolor(self, other):
+        return self.color == other.color
+    def eqshading(self, other):
+        return self.shading == other.shading
+    def eqsymbol(self, other):
+        return self.symbol == other.symbol
     
+    def _eq_(self, other):#==
+        if self.eqnumber(self, other) and self.eqcolor(self, other) and self.eqshading(self, other) and self.symbol(self, other) == True:
+            card.self = card.other 
+
 
 class set:
+    def _init_(self, cards):
+        self.cards = cards 
 
 
 
-
-
-
-    def __gt__(self, number, symbol, color, shading, num1, num2, num3,diamont, wave, oval, red, green, purple, solid, striped, empty):
-        
-    def _ne_(self, number, symbol, color, shading, num1, num2, num3,diamont, wave, oval, red, green, purple, solid, striped, empty):
-    
-    def _lt_(self, number, symbol, color, shading, num1, num2, num3,diamont, wave, oval, red, green, purple, solid, striped, empty):
 
 
 
