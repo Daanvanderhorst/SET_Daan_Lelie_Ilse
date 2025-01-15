9db283c7210b1d4645bdda2af8eef153c444cabe
Daanvanderhorst <daan_vanderhorst@hotmail.com>
2025-01-15 12:51:03 +0100

    Update Stap 1 -class.py

--- Stap 1 -class.py
+++ Stap 1 -class.py
@@ -1,48 +1,48 @@
 from enum import Enum 
-#https://www.geeksforgeeks.org/python-classes-and-objects/ 
 
-class color(Enum):
-    green = 0
-    purple = 1
-    red = 2
+class Color(Enum):
+    GREEN = 0
+    PURPLE = 1
+    RED = 2
 
-class symbol(Enum): 
-    diamond = 0
-    oval = 1
-    squiggle = 2
+class Symbol(Enum): 
+    DIAMOND = 0
+    OVAL = 1
+    SQUIGGLE = 2
 
-class shading(Enum):
-    empty = 0
-    filled = 1
-    shaded = 2
-
-class number(Enum):
-    1 = 1
-    2 = 2
-    3 = 3
+class Shading(Enum):
+    EMPTY = 0
+    FILLED = 1
+    SHADED = 2
         
-class card:
-    def __init__(self, color, symbol, shading, number):
-        self.color = color
-        self.symbol = symbol
-        self.shading = shading
-        self.number = number
+class Card:
+    def __init__(self, idnumber):
+        self.color = Color(idnumber//27)
+        self.symbol = Symbol(idnumber%27//9)
+        self.shading = Shading(idnumber%9//3)
+        self.number = idnumber%3+1
         self.list = [self.color, self.symbol, self.shading, self.number]
-
 
     def eqcolor(self, other):
         return self.color == other.color
+
     def eqsymbol(self, other):
         return self.symbol == other.symbol
+
     def eqshading(self, other):
         return self.shading == other.shading
+
     def eqnumber(self, other):
         return self.number == other.number
+
     def imagename(self):
-        imcolor = color(int(self.color)).name
-        return(imcolor)
+        imcolor = self.color.name.lower()
+        imsymbol = self.symbol.name.lower()
+        imshading = self.shading.name.lower()
+        return f"kaarten\\{imcolor}{imsymbol}{imshading}{self.number}.gif"
+    
     def _eq_(self, other):#==
-        if self.eqnumber(self, other) and self.eqcolor(self, other) and self.eqshading(self, other) and self.symbol(self, other) == True:
-            card.self = card.other 
-kaart = card(1,1,1,1)
-print(kaart.imagename)
+        return self.eqnumber(self, other) and self.eqcolor(self, other) and self.eqshading(self, other) and self.symbol(self, other)
+
+kaart = Card(1)
+print(kaart.imagename()) 
