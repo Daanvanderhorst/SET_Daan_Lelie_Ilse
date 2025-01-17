10b7e58ad23de1b23a8a96c07ba63e6a94fdd021
Daanvanderhorst <daan_vanderhorst@hotmail.com>
2025-01-17 19:52:45 +0100

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,22 +1,22 @@
 
 def checkforset(card1, card2, card3): #binnen drie kaarten kijken of er een set is
     for i in range(0,4): #je gaat alle verschillende eigenschappen af (4 eigenschappen)
-        if (card1.list[i].value + card2.list[i].value + card3.list[i].value)%3 != 0: #checken of alles niet hetzelfde of niet verschillend is 
+        if (card1[i] + card2[i] + card3[i])%3 != 0: #checken of alles niet hetzelfde of niet verschillend is 
             return False 
     return True #dan is het een set
 
-def listofsets(list): #binnen meerdere kaarten alle sets zoeken
+def listofsets(cards): #binnen meerdere kaarten alle sets zoeken
     sets = []  #een lege lijst maken voor sets die we later willen opslaan
-    for i in range(0, len(list)-2): 
-        for j in range(i+1, len(list)-1): 
-            for k in range(j+1, len(list)):
-                if checkforset(list[i]["kaart"], list[j]["kaart"], list[k]["kaart"]):
+    for i in range(0, len(cards)-2): 
+        for j in range(i+1, len(cards)-1): 
+            for k in range(j+1, len(cards)):
+                if checkforset(cards[i].list, cards[j].list, cards[k].list):
                     sets.append((i, j, k)) #de plus 1 is zodat je niet de index krijg maar echt het nummer van de fysieke kaart
     return sets #een lijst terug met alle sets uit de kaarten 
 
-def firstset(list): #dit vind gewoon een set en dan stopt het 
-    for i in range(0, len(list)-2):
-        for j in range(i+1, len(list)-1):
-            for k in range(j+1, len(list)):
-                if checkforset(list[i]["kaart"], list[j]["kaart"], list[k]["kaart"]):
+def firstset(cards): #dit vind gewoon een set en dan stopt het 
+    for i in range(0, len(cards)-2):
+        for j in range(i+1, len(cards)-1):
+            for k in range(j+1, len(cards)):
+                if checkforset(cards[i].list, cards[j].list, cards[k].list):
                     return ([k, j, i])
