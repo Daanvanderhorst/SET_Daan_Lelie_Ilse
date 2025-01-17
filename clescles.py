a0e1a951a5ec51f9459500a3fc624ea03e9ae23a
Daanvanderhorst <daan_vanderhorst@hotmail.com>
2025-01-17 20:00:49 +0100

    Update clescles.py

--- clescles.py
+++ clescles.py
@@ -15,28 +15,23 @@ class Shading(Enum):
     EMPTY = 0
     FILLED = 1
     SHADED = 2
-        
-class Number(Enum):
-    num1 = 1
-    num2 = 2
-    num3 = 3
 
 class Card:
     def __init__(self, idnumber, index, rect):
-        self.color = Color(idnumber//27)
-        self.symbol = Symbol(idnumber%27//9)
-        self.shading = Shading(idnumber%9//3)
-        self.number = Number(idnumber%3+1)
+        self.color = idnumber//27
+        self.symbol = idnumber%27//9
+        self.shading = idnumber%9//3
+        self.number = idnumber%3
         self.index = index
         self.rect = rect
         self.selected = False
-        self.list = [self.color.value, self.symbol.value, self.shading.value, self.number.value]
+        self.list = [self.color, self.symbol, self.shading, self.number]
     
     def getimage(self):
-        imcolor = self.color.name.lower()
-        imsymbol = self.symbol.name.lower()
-        imshading = self.shading.name.lower()
-        imnumber = self.number.value
+        imcolor = Color(self.color).name.lower()
+        imsymbol = Symbol(self.symbol).name.lower()
+        imshading = Shading(self.shading).name.lower()
+        imnumber = self.number + 1
         imagename = f"kaarten\\{imcolor}{imsymbol}{imshading}{imnumber}.gif"
         image = pygame.image.load(imagename)
         image = pygame.transform.scale(image, (100, 150))
